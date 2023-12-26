df9e1ae7a9169fedfd6f483d7a1e90c3c7384f60
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2023-12-26 17:32:08 +0200

    Testing what I finished so far

--- InOrder.java
+++ InOrder.java
@@ -2,8 +2,32 @@
  *  Generates and prints random integers in the range [0,10),
  *  as long as they form a non-decreasing sequence.
  */
+ 
+import java.util.*;
+
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
-	}
+		int Min = 0;
+		int Max = 10;
+		List<Integer> y = new ArrayList<Integer>();
+		Boolean n = true;
+		int x = Min + (int)(Math.random() * (Max - Min));
+		y.add(x);
+		int z;
+		while (n==true) {
+		z = Min + (int)(Math.random() * (Max - Min));
+			if (z >= x) {
+				y.add(z);
+				x = z;
+			} else {
+				n = false;
+			}
+		}
+		int size = y.size();
+		String yString = y.toString();
+		yString = yString.replace(",", "");
+		yString = yString.replace("[", "");
+		yString = yString.replace("]", "");
+		System.out.println(yString);
+	}	
 }
--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,17 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String x = args[0];
+		char[] y = x.toCharArray();
+		int length = x.length();
+		char[] z = new char[length];
+		for (int i = 0; i <= (length-1); i++) {
+			z[i] = y[length-(i+1)];
+		}
+		System.out.println(z);
+		char middle;
+		int even = ((length%2) == 0) ? (length/2 - 1) : (length/2);
+		middle = y[even];
+		System.out.println("The middle character is " + middle);
 	}
 }
