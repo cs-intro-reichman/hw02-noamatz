a9933ef3118024e799f098002eda68bfaaadbd6c
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2023-12-30 18:28:25 +0200

    Checking

--- Reverse.java
+++ Reverse.java
@@ -5,16 +5,11 @@
 public class Reverse {
 	public static void main (String[] args){
 		String x = args[0];
-		char[] y = x.toCharArray();
-		int length = x.length();
-		char[] z = new char[length];
-		for (int i = 0; i <= (length-1); i++) {
-			z[i] = y[length-(i+1)];
+		int z = x.length();
+		for (int i = 0; i < z; i++) {
+			System.out.print(x.charAt((z - i) - 1));
 		}
-		System.out.println(z);
-		char middle;
-		int even = ((length%2) == 0) ? (length/2 - 1) : (length/2);
-		middle = y[even];
-		System.out.println("The middle character is " + middle);
+		System.out.println("");
+		System.out.println("The middle character is " + (x.charAt(z / 2)));
 	}
 }
