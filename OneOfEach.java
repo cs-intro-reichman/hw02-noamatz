c3aeca9fbb7a73cfc45c631482144cdf6b6892da
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2023-12-30 22:27:15 +0200

    Done :)

--- OneOfEach.java
+++ OneOfEach.java
@@ -3,78 +3,52 @@
  *  Simulates the formation of a family in which the parents decide 
  *  to have children until they have at least one child of each gender.
  */
-
-import java.util.*; 
+import java.lang.Math;
 
 public class OneOfEach {
-	static List<String> y = new ArrayList<>();
-	static List<Integer> z = new ArrayList<Integer>();
-	static int TwoChildren = 0;
-	static int ThreeChildren = 0;
-	static int FourChildren = 0;
-	static int sizeZ;
-	static int sum;
-
 	public static void main(String[] args) {
-		int T = Integer.parseInt(args[0]);
-		double average;
-		for (int i = 0; i < T; i++) {
-			Family();
-		}
-		int size = y.size();
-		average = (double)sum/(double)T;
-		System.out.println(y);
-		System.out.println((double)size);
-		System.out.println((double)T);
-		System.out.println("Average: " + average + " children to get at least one of each gender.");
-		System.out.println("Number of families with 2 children: " + TwoChildren);
-		System.out.println("Number of families with 3 children: " + ThreeChildren);
-		System.out.println("Number of families with 4 or more children: " + FourChildren);
-		if ((TwoChildren > ThreeChildren) && (TwoChildren > FourChildren)) {
-			System.out.println("The most common number of children is 2.");
-		} else if ((ThreeChildren > TwoChildren) && (ThreeChildren > FourChildren)) {
-			System.out.println("The most common number of children is 3.");
-		} else {
-			System.out.println("The most common number of children is 4 or more.");
-		}	
-	}
-	
-	static void Family() {
+		int sum = 0;
+		int TwoChildren = 0;
+		int ThreeChildren = 0;
+		int FourChildren = 0;
 		int Min = 0;
 		int Max = 1;
 		int x = Min + (int)(Math.random() * ((Max - Min) + 1));
 		Boolean boy = false; 
 		Boolean girl = false;
-		z.clear();
+		int boyNum = 0;
+		int girlNum = 0;
+		int famSize;
+		String fam = "";
 		if (x == 0) {
 			boy = true;
-			y.add("b");
-			z.add(0);
+			boyNum++;
+			fam = "b ";
 		} else {
 			girl = true;
-			y.add("g");
-			z.add(1);
+			girlNum++;
+			fam = "g ";
 		}
 		Boolean n = false;
 		while (n == false) {
 			x = Min + (int)(Math.random() * ((Max - Min) + 1));
 			if (x == 0) {
 				if (boy == true) {
-					y.add("b");
-					z.add(0);
+					boyNum++;
+					fam = fam + "b ";
 				} else if (girl == true) {
-					y.add("b");
-					z.add(0);
+					boyNum++;
+					fam = fam + "b ";
 				} else {
 					n = true;
 				}	
 			} else if (x == 1) {
 				if (girl == true) {
-					y.add("g");
-					z.add(1);
+					girlNum++;
+					fam = fam + "g ";
 				} else if (boy == true) {
-					y.add("g");
-					z.add(1);
+					girlNum++;
+					fam = fam + "g ";
 				} else {
 					n = true;
 				}
@@ -82,17 +56,21 @@ public class OneOfEach {
 				n = true;
 			}
 		}
-		System.out.println(y + "now");
-		sizeZ = z.size();
-		if (sizeZ <= 2) {
+		famSize = boyNum + girlNum;
+		if (famSize <= 2) {
 			TwoChildren++;
 			sum = sum + 2;
-		} else if (sizeZ == 3) {
+		} else if (famSize == 3) {
 			ThreeChildren++;
 			sum = sum + 3;
 		} else {
 			FourChildren++;
-			sum = sum + z.size();
+			sum = sum + famSize;
 		}
+		System.out.println("famSize: " + famSize);
+		System.out.println("TwoChildren: " + TwoChildren);
+		System.out.println("ThreeChildren: " + ThreeChildren);
+		System.out.println("FourChildren: " + FourChildren);
+		System.out.println("Fam: " + fam);
 	}
 }
